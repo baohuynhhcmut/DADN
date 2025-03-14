6d5648c840904aa885149bcb1faee59505e6ed0a
Bao Huynh Gia <bao.huynh2201@hcmut.edu.vn>
2025-03-14 23:50:26 +0700

    page home

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,21 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../pages/Login/Login";
-import Home from "../pages/Home";
+import Home from "../pages/Home/Home";
 import LayoutUser from "../layout/user";
+import Devices from "../pages/Devices";
+import Profile from "../pages/Profile";
 
 export const router = createBrowserRouter([
-    {
-        path:"/login",
-        element: <Login />
-    },
-    {
-        path: '/',
-        element: <LayoutUser />,
-        children:[
-            {
-                index:true,
-                element: <Home />
-            }
-        ]
-    }
-])
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/",
+    element: <LayoutUser />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "devices",
+        element: <Devices />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />
+      }
+    ],
+  },
+]);
