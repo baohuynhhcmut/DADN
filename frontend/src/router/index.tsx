ae5c5e9a8b4c36169655268be17ac583cd02a7f4
Panhdthg <peiuamnhac@gmail.com>
2025-03-14 23:48:30 +0700

    i
    i

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../pages/Login/Login";
 import Home from "../pages/Home";
+import Devices from "../pages/Devices";
 import LayoutUser from "../layout/user";
 
 export const router = createBrowserRouter([
@@ -15,6 +16,16 @@ export const router = createBrowserRouter([
             {
                 index:true,
                 element: <Home />
+            }
+        ]
+    },
+    {
+        path: '/devices',
+        element: <LayoutUser />,
+        children:[
+            {
+                index:true,
+                element: <Devices />
             }
         ]
     }
